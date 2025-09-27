c8e185fcd7c4a8931125808ae22252612a7b4826
IBESTISHERE <107686752+ibestishere@users.noreply.github.com>
2025-09-27 16:07:06 +0700

    script.js

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const body = document.body;
 themeBtn.addEventListener("change", () => {
   body.classList.toggle("night");
 });
+/*
 document.querySelectorAll('.plam').forEach(card => {
   card.addEventListener('click', () => {
     window.location.href = 'player/plam/plam.html';
   });
+
 });
+*/
