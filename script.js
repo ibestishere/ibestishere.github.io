2dec9bab580935274de6a4d2497588d61ffbffbb
IBESTISHERE <107686752+ibestishere@users.noreply.github.com>
2025-09-27 17:08:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ const body = document.body;
 themeBtn.addEventListener("change", () => {
   body.classList.toggle("night");
 });
+
 /*
 document.querySelectorAll('.plam').forEach(card => {
   card.addEventListener('click', () => {
     window.location.href = 'player/plam/plam.html';
   });
-
 });
 */
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+  const originalHTML = card.innerHTML; // เก็บค่าเดิมของการ์ด
+  card.addEventListener('click', () => {
+    // ใส่ข้อความใหม่
+    card.innerHTML += `<p class="card-message">ยังทำไม่เสร็จ! รอก่อน</p>`;
+    // ผ่านไป 5 วิ กลับเป็นค่าเดิม
+    setTimeout(() => {
+      card.innerHTML = originalHTML;
+    }, 5000);
+  });
+});
