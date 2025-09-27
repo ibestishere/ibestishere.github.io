4426fd8d6e0965fecad7b16082acae3b6ba67edf
IBESTISHERE <107686752+ibestishere@users.noreply.github.com>
2025-09-27 14:54:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.querySelector(".checkbox");
+const body = document.body;
+
+themeBtn.addEventListener("change", () => {
+  body.classList.toggle("night");
+});
+document.querySelectorAll('.plam').forEach(card => {
+  card.addEventListener('click', () => {
+    window.location.href = 'player/plam/plam.html';
+  });
+});
